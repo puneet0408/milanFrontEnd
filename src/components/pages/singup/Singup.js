c8d0c593c16af905962c1e53f5e44f1e954d33c5
Nikhil Walia <nikhilwalia363@gmail.com>
2022-11-09 15:00:26 +0530

    form scroll bar added

--- src/components/pages/singup/Singup.js
+++ src/components/pages/singup/Singup.js
@@ -1,20 +1,20 @@
 import { React,useState } from "react"
+import { Form } from "react-router-dom";
 import img from '../../../static/singup/formleft.jpg'
 import './Singup.css'
 const Singup = () => {
-    let form = 
+
+    let [FormData,setFormData] = useState();
+    let [error,setError] = useState();
+    let handleChange = (event) => 
     {
-        username:"",
-        password:"",
-        confirm_password:"",
-        name:"",
-        birthDay:"",
-        gender:"",
-        desire_gender:"",
-        marital_statis:"",
-        City:""
+        let {name,value} = event.target;
+        setFormData({...FormData, [name]:value});
     }
-    let [FormData,setFomrmData] = useState(form);
+    let validate = (event) =>
+    {
+        console.log(event.target.name);
+    }
     return (
         <div className="sign">
             <div className="container-fluid">
@@ -25,7 +25,7 @@ const Singup = () => {
                     </div>
                 </div>
                 <div className="col-xl-7 col-lg-6 col-sm-12 px-5 py-3">
-                    <div className="sing_form">
+                    <form className="sing_form">
                         <div className="sing_header">
                             <h2>
                             WELCOME TO MILAN
@@ -39,19 +39,37 @@ const Singup = () => {
                                     <h3>Account Details</h3>
                                 </div>
                                 <div class="input-group mb-3">
-                                    <label for="" className="mb-2">Username*</label>
-                                    <input type="text" className="sing_my_control" placeholder="Enter Your Username"/>
+                                    <label  className="mb-2">Username*</label>
+                                    <input type="text" className="sing_my_control" placeholder="Enter Your Username" onChange={handleChange} onBlur={validate} name="username"/>
                                 </div>
                                 <div class="input-group mb-3">
-                                    <label for="" className="mb-2">Password*</label>
-                                    <input type="text" className="sing_my_control" placeholder="Enter Your Password"/>
+                                    <label  className="mb-2">Password*</label>
+                                    <input type="text" className="sing_my_control" placeholder="Enter Your Password" name="passoword" onChange={handleChange} onBlur={validate}/>
                                 </div>
                                 <div class="input-group mb-3">
-                                    <label for="" className="mb-2">Confirm Password*</label>
-                                    <input type="text" className="sing_my_control" placeholder="Enter Your Password"/>
+                                    <label className="mb-2">Confirm Password*</label>
+                                    <input type="text" className="sing_my_control" placeholder="Enter Your Password" name="confirm_password:" onChange={handleChange} onBlur={validate}/>
                                 </div>
                             </div>
-                    </div>
+                            <div className="profile_details">
+                                <div className="content_title">
+                                    <h3>Profile Details</h3>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <label  className="mb-2">Username*</label>
+                                    <input type="text" className="sing_my_control" placeholder="Enter Your Full Name" onChange={handleChange} onBlur={validate} name="name"/>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <label  className="mb-2">DOB*</label>
+                                    <input type="date" className="sing_my_control" placeholder="mm/dd/yy" name="birthday" onChange={handleChange} onBlur={validate}/>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <label  className="mb-2">City*</label>
+                                    <input type="text" className="sing_my_control" placeholder="Enter Your City" name="city" onChange={handleChange} onBlur={validate}/>
+                                </div>
+                            </div>
+                            <button type="submit" className="btn custom_btn acc_btn text-nowrap">Create Account</button>
+                    </form>
                 </div>
             </div>
         </div>
