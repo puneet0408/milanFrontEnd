8f460d89059c94fa7459946a081bbbcb90835107
bhavyagg90 <gbhavya18sliet@gmail.com>
2022-12-07 00:51:30 +0530

    dezining

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //Import Pages
-import Nav from '../src/components/nav/Nav'
-import Home from './components/Home/Home'
-import Life from './components/pages/life/Life';
-import Shop from './components/pages/shop/Shop';
-import Profile from './components/pages/profile/Profile';
-import Singup from './components/pages/singup/Singup';
-import Chat  from './components/Chat/Chat';
-import Singin from './components/pages/singin/Singin';
+import Nav from "../src/components/nav/Nav";
+import Home from "./components/Home/Home";
+// import Life from "./components/pages/life/Life";
+// import Shop from './components/pages/shop/Shop';
+import Profile from "./components/pages/profile/Profile";
+import Singup from "./components/pages/singup/Singup";
+import Chat from "./components/Chat/Chat";
+import Singin from "./components/pages/singin/Singin";
 function App() {
   return (
     <BrowserRouter>
-   <Nav/>
-    <Routes>
-      <Route path='/' index element={<Home/>} />
-      <Route path='/life' element={<Life/>}/>
-      <Route path='/shop' element={<Shop/>}/>
-      <Route path='/profile' element={<Profile/>}/>
-      <Route path='/singup' element={<Singup/>}/>
-      <Route path='/chat' element={<Chat/>}></Route>
-      <Route path='/singin' element={<Singin/>}></Route>
-    </Routes>
+      <Nav />
+      <Routes>
+        <Route path="/" index element={<Home />} />
+        {/* <Route path="/life" element={<Life />} /> */}
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/singup" element={<Singup />} />
+        <Route path="/chat" element={<Chat />}></Route>
+        <Route path="/singin" element={<Singin />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,124 +1,121 @@
-import { React, useState, useEffect, useCallback } from "react"
-import { useDispatch, useSelector } from "react-redux"
+import { React, useState, useEffect, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 //Import Reducers Action
-import { login, logout } from "../../store/login/login"
-import {make_connection,close_connecton} from "../../store/socket/socket"
-import {clear_messages,insert_new_messages} from "../../store/Messages/messages"
-import {set_contacts,clear_contacts} from "../../store/Users/users"
-import {clearTarget,setTarget} from "../../store/OpenChat/target"
+import { login, logout } from "../../store/login/login";
+import { make_connection, close_connecton } from "../../store/socket/socket";
+import {
+  clear_messages,
+  insert_new_messages,
+} from "../../store/Messages/messages";
+import { set_contacts, clear_contacts } from "../../store/Users/users";
+import { clearTarget, setTarget } from "../../store/OpenChat/target";
 
 //Import Individual Componets
-import ChatWindow from './ChatWindow/Chatwindow'
+import ChatWindow from "./ChatWindow/Chatwindow";
 
-
-import { io } from 'socket.io-client'
-import axios from "axios"
-import './Chat.css'
+import { io } from "socket.io-client";
+import axios from "axios";
+import "./Chat.css";
 let Chat = () => {
   //Redux Use redux react toolkit
   const islogin = useSelector((state) => state.login.value);
   const dispatch = useDispatch();
 
   //Set Open Messages
-  let reducerMsgs = useSelector((state)=> state.messages.value)
-  
+  let reducerMsgs = useSelector((state) => state.messages.value);
 
-
-
-  const [socketid,setSocketid] = useState({});
+  const [socketid, setSocketid] = useState({});
   const [socket, setSocket] = useState(null);
   const [chats, setChats] = useState([]);
-  const [messages,setMessages] = useState([]);
-  const [reciver,setReciver] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [reciver, setReciver] = useState("");
   //Store State of Current Open Chat Id
-  const [openchatid,setOpencahtid] = useState('');
+  const [openchatid, setOpencahtid] = useState("");
 
-  useEffect(()=>
-  {
+  useEffect(() => {
     console.log(chats);
 
     dispatch(set_contacts(chats));
-  },[chats]);
+  }, [chats]);
 
   //State for Chat Message Box
-  const [msg,setMsg] = useState("");
-  const handlechange = (event) => 
-  {
+  const [msg, setMsg] = useState("");
+  const handlechange = (event) => {
     setMsg(event.target.value);
-  }
+  };
 
   //Make Web Socket Connection With Backend
   useEffect(() => {
-    const newsocket = io("127.0.0.1:4000",
-      {
-        auth:
-        {
-          userid: localStorage.getItem('id'),
-          token: localStorage.getItem('token')
-        }
-      });
+    const newsocket = io("127.0.0.1:4000", {
+      auth: {
+        userid: localStorage.getItem("id"),
+        token: localStorage.getItem("token"),
+      },
+    });
     setSocket(newsocket);
     dispatch(clear_messages());
     //Close Connection on Leaving
     return () => {
       newsocket.close();
-    }
+    };
   }, []);
 
-  useEffect(()=> 
-  {
-    dispatch(make_connection(socket))
-  },[socket]);
-
+  useEffect(() => {
+    dispatch(make_connection(socket));
+  }, [socket]);
 
   //Fetch User (Friends List)
   useEffect(() => {
     var data = JSON.stringify({
-      "id": localStorage.getItem('id'),
-      "token": localStorage.getItem('token')
+      id: localStorage.getItem("id"),
+      token: localStorage.getItem("token"),
     });
 
     var config = {
-      method: 'post',
-      url: 'http://localhost:4000/backendapi/chat/fetchcontact',
+      method: "post",
+      url: "http://localhost:4000/backendapi/chat/fetchcontact",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      data: data
+      data: data,
     };
 
     axios(config)
       .then(function (response) {
         setChats(response.data);
-        setSocketid(JSON.parse(localStorage.getItem('Chats')));
+        setSocketid(JSON.parse(localStorage.getItem("Chats")));
       })
       .catch(function (error) {
         console.log(error);
       });
-
   }, []);
-
 
   //Helper Function that Return userDetails Window
   const userDetails = (user) => {
     return (
-      <div className="user_details" onClick={(event)=> {openMessageWindow(event,user._id)}}>
-        <div  className="photo"><img src={user.photo} /></div>
+      <div
+        className="user_details"
+        onClick={(event) => {
+          openMessageWindow(event, user._id);
+        }}
+      >
+        <div className="photo">
+          <img src={user.photo} />
+        </div>
         <div className="user_details_name_time">
           <div className="name">
             <div>{user.name}</div>
             <div>Time</div>
           </div>
-          <div className="last_message">
-            hiii
-          </div>
+          <div className="last_message">hiii</div>
         </div>
-      </div>);
-  }
+      </div>
+    );
+  };
 
-  //Individual Message 
-  // const chat = (message) => 
+  //Individual Message
+  // const chat = (message) =>
   // {
   //     return (
   //       //Set Class Based on (Message Varrible Vales)
@@ -132,7 +129,7 @@ let Chat = () => {
   // }
 
   //Chat Area
-  // const userChats = (Chats) => 
+  // const userChats = (Chats) =>
   // {
   //     return (<div className="chat_details">
   //       <div className="oldchat">
@@ -145,59 +142,56 @@ let Chat = () => {
   //     </div>);
   // }
 
-  const openMessageWindow = (event,rec) => {
+  const openMessageWindow = (event, rec) => {
     //Set Target User Details
-    let ob={id:rec,chatid:socketid[rec]};
+    let ob = { id: rec, chatid: socketid[rec] };
     dispatch(setTarget(ob));
 
     setOpencahtid(socketid[rec]);
-    socket.on('message',(data)=> 
-    {
+    socket.on("message", (data) => {
       setMessages(data.conversations);
-      dispatch(insert_new_messages(data.conversations))
+      dispatch(insert_new_messages(data.conversations));
       setReciver(rec);
-    })
-    socket.emit('fetch_last_message',socketid[rec],'2');
+    });
+    socket.emit("fetch_last_message", socketid[rec], "2");
     // socket.emit('create_message','637b4e231a68c0c26f074a85','637b4de71a68c0c26f074a83','Hii Brother');
     // socket.emit('unseen_message','637b47dc51126d4f5d350d78','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzN2I0N2RjNTExMjZkNGY1ZDM1MGQ3OCIsImlhdCI6MTY2OTI2NjAyOH0.w8-OpdEvMAf-zEM8762dph9l6c-_UFS94YYzQ3KRyKo');
-  }
+  };
 
   //Send New Message
-  const send_message = () =>
-  {
-      let sender=localStorage.getItem('id');
-      socket.emit('create_message',sender,reciver,msg);
-  }
+  const send_message = () => {
+    let sender = localStorage.getItem("id");
+    socket.emit("create_message", sender, reciver, msg);
+  };
 
   //Litening for New Message
-  socket && socket.on('new_message',(ms)=> 
-  {
-      setMessages([...messages,ms]);
-  });
-
+  socket &&
+    socket.on("new_message", (ms) => {
+      setMessages([...messages, ms]);
+    });
 
   //Testing Mark as seen Feature
-  const mark_message = (chatid,msgid) => 
-  {
-    socket.volatile.emit('mark_as_read',chatid,msgid);
-  }
-
+  const mark_message = (chatid, msgid) => {
+    socket.volatile.emit("mark_as_read", chatid, msgid);
+  };
 
   //Join All rooms
-  socket && chats.length>0 && socket.emit('join_all_rooms',localStorage.getItem('id'));
+  socket &&
+    chats.length > 0 &&
+    socket.emit("join_all_rooms", localStorage.getItem("id"));
 
-  return (<div>
-    <div className="container-fluid chat-container">
-      <div className="row">
-        <div className="col-md-6 col-sm-12">
-          {chats.map(userDetails)}
-        </div>
-        <div className="col-md-6 col-sm-12">
-          {/* {userChats(messages)} */}
-          <ChatWindow/>
+  return (
+    <div>
+      <div className="container-fluid chat-container">
+        <div className="row">
+          <div className="col-md-6 col-sm-12">{chats.map(userDetails)}</div>
+          <div className="col-md-6 col-sm-12">
+            {/* {userChats(messages)} */}
+            <ChatWindow />
+          </div>
         </div>
       </div>
     </div>
-  </div>);
-}
+  );
+};
 export default Chat;
